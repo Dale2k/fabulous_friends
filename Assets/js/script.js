2eff6f8099b84e4a932372049737e2ba514a4ed9
Darryl Subal <dsubal@att.net>
2023-03-23 11:08:30 -0400

    add local storage and more cat selects

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,42 @@
 var apiURL = "https://dog.ceo/api/breed/";
-var baseURL = 'https://dog.ceo/api/breed/';
-var formatInput = document.querySelector('#format');
+var baseURL = "https://dog.ceo/api/breed/";
+var formatInput = document.querySelector("#format");
 // var format = formatInput.value.trim();
-console.log(format);
-var formatInputCat = document.querySelector('#formatCat')
-var starBtn = document.querySelector('.btn1')
-var starBtn2 = document.querySelector('.btn2')
+// console.log(format);
+var formatInputCat = document.querySelector("#formatCat");
+var starBtn = document.querySelector(".btn1");
+var starBtn2 = document.querySelector(".btn2");
 var cardLink = document.createElement("img");
 // var catBaseApi = 'https://api.thecatapi.com/v1/breeds';
-var catBasic2 = 'https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=' + formatInputCat.value.trim() + '&api_key=REPLACE_ME'
 
+// var dogShowUrl = document.getElementById('a');
+// dogShowUrl = dogShowUrl.value.url;
+
+var catBasic2 =
+  "https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=" +
+  formatInputCat.value.trim() +
+  "&api_key=REPLACE_ME";
+
+// local storage dog
+var getSearchedDog = function () {
+  return JSON.parse(localStorage.getItem("searchedDog")) || [];
+};
+
+var setSearchedDog = function (text) {
+  var searchedDog = getSearchedDog();
+  searchedDog.push(text);
+  localStorage.setItem("searchedDog", JSON.stringify(searchedDog));
+};
+
+//  local storage Cat
+var getSearchedCat = function () {
+  return JSON.parse(localStorage.getItem("searchedCat")) || [];
+};
+var setSearchedCat = function (text) {
+  var searchedCat = getSearchedCat();
+  searchedCat.push(text);
+  localStorage.setItem("searchedCat", JSON.stringify(searchedCat));
+};
 
 var toJSON = function (response) {
   // console.log(response);
@@ -17,12 +44,13 @@ var toJSON = function (response) {
 };
 var idEl = document.querySelector("#id");
 
-var renderCats = function (data) {
+var renderDogs = function (data) {
   console.log(data);
   // for (i = 0; i < data.message.length; i++) {
   // var cardLink = document.createElement("img");
   cardLink.setAttribute("src", data.message);
   idEl.appendChild(cardLink);
+  setSearchedDog(data.message);
   // }
 };
 
@@ -38,14 +66,14 @@ var renderCats = function (data) {
 // }
 function dogBreed() {
   var format = formatInput.value.trim();
-  console.log(format)
+  console.log(format);
   // if (format) {
-  apiURL = baseURL + format + '/images/random'
+  apiURL = baseURL + format + "/images/random";
   console.log(apiURL);
   // }
   fetch(apiURL)
     .then(toJSON)
-    .then(renderCats)
+    .then(renderDogs)
     .catch(function (err) {
       console.log(err);
     });
@@ -56,19 +84,21 @@ function dogBreed() {
   // }).then(function(data){
   //   console.log(data);
   // })
-};
-starBtn.addEventListener('click', dogBreed)
+}
+starBtn.addEventListener("click", dogBreed);
 // dogBreed()
 
 function catBreed() {
   fetch(catBasic2)
     .then(function (response) {
-      return response.json()
-    }).then(function (data) {
+      return response.json();
+    })
+    .then(function (data) {
       console.log(data);
       cardLink.setAttribute("src", data[0].url);
       idEl.appendChild(cardLink);
+      setSearchedCat(data[0].url);
     });
 }
 
-starBtn2.addEventListener('click', catBreed)
+starBtn2.addEventListener("click", catBreed);
